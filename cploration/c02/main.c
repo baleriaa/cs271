3bc1b798f58258184089a936a1dd884a58e66b19
Baleria Reyes <breyes815@gmail.com>
2021-10-05 20:10:20 -0700

    c02 directory added

---
+++ cploration/c02/main.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdio.h>
+#define MAX_LIMIT 100;
+
+int main()
+{
+/** WARMUP **/
+// declare a string with value
+char hello[] = "Hello";
+// declare an empty string
+char name[MAX_LIMIT];
+
+// prompt user
+printf("What is your name? ");
+
+// read in a string from user and save in variable
+// [^\n] means to discard the newline character
+scanf("%[^\n]s", name);
+
+// print out "hello <name>"
+printf("%s %s!\n\n", hello, name);
+
+/** Exercise 1 **/
+
+char welcome[MAX_LIMIT];
+welcome = hello;
+}
