0eff9eebc1befe1cd9056b31db257d822cd9e0cf
breyes815@gmail.com <breyes815@gmail.com>
2021-10-24 23:06:01 -0700

    finished.

--- parser.c
+++ parser.c
@@ -1,43 +1,31 @@
 #include "parser.h"
 
-void parse (FILE * file)
-{
-	
-}
+void parse (FILE * file){
+	char line[MAX_LINE_LENGTH] = "";
 
-char line[strlen(s)];
-
-while(fgets(line, sizeof(line), fin))
-{
+	while(fgets(line, sizeof(line), file)){
        strip(line);
-	if (line == NULL)
-	{
-		continue;
-	}
-	else
-	{
-		printf("%s\n", line);
-	}
-	printf(line);	
+		if (*line == '\0'){
+			continue;
+		}
+		else{
+			printf("%s\n", line);
+		}
+	}	
 }
 
-char *strip(char *s)
-{
-	char s_new[s + 1];
+char *strip(char *s){
+	char s_new[sizeof(s) + 1];
 	int i = 0;
-	for (char *s2 = s; *s2; s2++)
-       {
-   		if (*s2 && *(s+1) == "/")
-		{
+	for (char *s2 = s; *s2; s2++){
+   		if (*s2 && *(s2+1) == "/"){
 			break;
-		}
-		else if (!= isspace(*s2))
-		{
+		} 
+		else if (!isspace(*s2)){
 			s_new[i++] = *s2;	
 		}
-       }
+	}
 	s_new[i] = '\0';
 	stpcpy(s, s_new);
 	return s;
-
 }
