579d0e285d429c433a2c8cc9e2a98c9434d5cdca
baleriaa <43627339+baleriaa@users.noreply.github.com>
2021-11-11 13:10:13 -0800

    Add files via upload

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -9,4 +9,30 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+@R2
+M=0
+
+@R0
+D=M
+@END
+D;JEQ
+
+@R0
+D=M
+@R3
+M=D
+
+(LOOP)
+@R1
+D=M
+
+@R2
+M=D+M
+
+@R3
+M=M-1
+
+D=M
+@LOOP
+D;JGT
+
