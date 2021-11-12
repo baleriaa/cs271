b832c3bf0b0a7481ce88a5453cfee790a4c4c71e
Baleria <breyes815@gmail.com>
2021-11-12 09:46:55 -0800

    Exxercise 2 done. Maybe with some errors.

--- cploration/c07/symtable.h
+++ cploration/c07/symtable.h
@@ -9,3 +9,14 @@ SYMBOL_TABLE_SIZE 100
 
 typdef int16_t hack_addr
 
+typedef struct Symbol {
+	char name[SYMBOL_TABLE_SIZE];
+	int addr;
+}
+
+Symbol* hashArray[SYMBOL_TABLE_SIZE];
+
+int hash(char *str);
+struct Symbol *find(char * key);
+void insert(char* key, hack_addr addr);
+void display_table();
