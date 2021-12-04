8237a314a4328e9dc3afea54525b915c55f9d8f6
Baleria <breyes815@gmail.com>
2021-12-04 12:07:32 -0800

    Fixed exercises 3 and 4.

--- cploration/c10/hack.h
+++ cploration/c10/hack.h
@@ -54,3 +54,124 @@ static const predefined_symbol predefined_symbols[NUM_PREDEFINED_SYMBOLS] = {
 	{"16384", SYM_SCREEN},
 	{"24676", SYM_KBD}
 };
+
+typedef enum jump_id {
+	JMP_INVALID = -1,
+	JMP_NULL = 0,
+	JMP_JGT = 1,
+	JMP_JEQ = 2,
+	JMP_JGE = 3,
+	JMP_JLT = 4,
+	JMP_JNE = 5,
+	JMP_JLE = 6,
+	JMP_JMP = 7
+} jump_id;
+
+typedef enum dest_id {
+	DEST_INVALID = -1,
+	DEST_NULL = 0,
+	DEST_M = 1,
+	DEST_D = 2,
+	DEST_DM = 3,
+	DEST_A = 4,
+	DEST_AM = 5,
+	DEST_AD = 6,
+	DEST_ADM = 7
+} dest_id;
+
+typedef enum comp_id {
+	COMP_INVALID = -1,
+	COMP_0 = 42,
+	COMP_1 = 63,
+	COMP_D = 12,
+	COMP_A = 48,
+	COMP_M = 48,
+	COMP_NOT_D = 13,
+	COMP_NOT_A = 49,
+	COMP_NOT_M = 49,
+	COMP_NEG_D = 15,
+	COMP_NEG_A = 51,
+	COMP_NEG_M = 51,
+	COMP_D_PLUS_1 = 31,
+	COMP_A_PLUS_1 = 55,
+	COMP_M_PLUS_1 = 55,
+	COMP_D_MINUS_1 = 14,
+	COMP_A_MINUS_1 = 50,
+	COMP_M_MINUS_1 = 50,
+	COMP_D_OPLUS_A = 2,
+	COMP_D_PLUS_M = 2,
+	COMP_D_MINUS_A = 19,
+	COMP_D_MINUS_M = 19,
+	COMP_D_AND_A = 0,
+	COMP_D_AND_M = 0,
+	COMP_D_OR_A = 21,
+	COMP_D_OR_M = 21
+} comp_id;
+
+static inline jump_id str_to_jumpid(const char *s) {
+	jump_id id = JMP_INVALID;
+	if (s == NULL) {
+		id = JMP_NULL;
+	}
+	else if (strcmp(s, "JGT") == 0) {
+		id = JMP_JGT;
+	}
+	else if (strcmp(s, "JEQ") == 0) {
+		id = JMP_JEQ;
+	}
+	else if (strcmp(s, "JGE") == 0) {
+		id = JMP_JGE;
+	}
+	else if (strcmp(s, "JLT") == 0) {
+		id = JMP_JLT;
+	}
+	else if (strcmp(s, "JNE") == 0) {
+		id = JMP_JNE;
+	}
+	else if (strcmp(s, "JLE") == 0) {
+		id = JMP_JLE;
+	}
+	else if (strcmp(s, "JMP") == 0) {
+		id = JMP_JMP;
+	}
+	return id;
+}
+
+static inline comp_id str_to_destid(const char *s) {
+	dest_id id = DEST_INVALID;
+	if (s == NULL) {
+		id = DEST_NULL;
+	}
+	else if (strcmp(s, "M") == 0) {
+		id = DEST_M;
+	}
+	else if (s == "D") {
+		id = DEST_D;
+	}
+	else if (s == "DM") {
+		id = DEST_DM;
+	}
+	else if (s == "A") {
+		id = DEST_A;
+	}
+	else if (s == "AM") {
+		id = DEST_AM;
+	}
+	else if (s == "AD") {
+		id = DEST_AD;
+	}
+	else if (s == "ADM") {
+		id = DEST_ADM;
+	}
+	return id;
+}
+
+static inline comp_id str_to_compid(const char *s, int *a){
+	comp_id id = COMP_INVALID;
+	if (s == NULL){
+		id = 42;
+	}
+	else if (s == ){
+		id = 
+	}
+}
