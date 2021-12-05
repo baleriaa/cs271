03874d2fbcdff91fc926eb44a6150c55394faf38
Baleria <breyes815@gmail.com>
2021-12-05 12:59:59 -0800

    Exercise 10 done.

--- cploration/c10/main.c
+++ cploration/c10/main.c
@@ -3,7 +3,10 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+#define MAX_INSTRUCTION_COUNT 30000
+
 int main (int argc, const char *argv[]){
+	instruction *instructions = malloc(MAX_INSTRUCTION_COUNT * sizeof(instruction));
 	if(argc != 2)
 	{
 		//incorrect number of arguments
@@ -16,6 +19,9 @@ int main (int argc, const char *argv[]){
 	{
 		exit_program(EXIT_CANNOT_OPEN_FILE, argv[1]);
 	}
-	parse(fin);
+	parse(fin, instructions);
+	int num_instructions;
+	num_instructions = parse(fin, instructions);
 	fclose(fin);
+	free(instructions);
 }
